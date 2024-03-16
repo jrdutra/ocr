b7d714f4ea6a8a1730d8d12dc443a27037dc6e44
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-03-16 20:42:11 -0300

    Tirando pipeline

--- src/main/java/com/ferramentas/ocr/OcrApplication.java
+++ src/main/java/com/ferramentas/ocr/OcrApplication.java
@@ -2,9 +2,10 @@ package com.ferramentas.ocr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OcrApplication {
+public class OcrApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrApplication.class, args);
