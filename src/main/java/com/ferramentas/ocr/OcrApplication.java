8acf8acdd5dc9de435ff9c141701177be2bfd51a
João Ricardo Côre Dutra <jrdutra_@msn.com>
2024-03-16 21:37:45 -0300

    Tirando pipeline

--- src/main/java/com/ferramentas/ocr/OcrApplication.java
+++ src/main/java/com/ferramentas/ocr/OcrApplication.java
@@ -1,8 +1,15 @@
 package com.ferramentas.ocr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import net.sourceforge.tess4j.util.LoadLibs;
 
 @SpringBootApplication
 public class OcrApplication extends SpringBootServletInitializer {
@@ -10,5 +17,26 @@ public class OcrApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(OcrApplication.class, args);
 	}
+	
+	@Component
+    public static class TessInitializer {
+
+        @Autowired
+        private ResourceLoader resourceLoader;
+
+        @EventListener(ApplicationReadyEvent.class)
+        public void initTess4j() {
+            try {
+                // Extrai os recursos do Tesseract para uma pasta temporária
+                // e configura o caminho da biblioteca nativa do Tesseract
+                // para essa pasta temporária
+                System.setProperty("java.library.path", 
+                    LoadLibs.extractTessResources("win32-x86-64").getPath());
+            } catch (Exception e) {
+                // Lidar com a exceção, se necessário
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
